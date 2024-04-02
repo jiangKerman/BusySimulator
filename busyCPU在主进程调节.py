1ceb4474cc7bd4dd64e4eda1f53ee8a283b7f7cf
jiangKerman <jiangkerman@outlook.com>
2024-04-02 17:54:17 +0800

    现在可以控制cpu波动在一个范围内，多退少补

--- busyCPU在主进程调节.py
+++ busyCPU在主进程调节.py
@@ -20,24 +20,28 @@ def occupy_cpu(percent, cpu_index, usage_list):
     full_time = 1  # 运行周期
     runtime = percent / 100
     sleep_time = full_time - runtime
+    # time.sleep(1)
+    total_rate = usage_list[cpu_index]
+    # print(total_rate)
+
+    # time_start = time.time()  # 记录开始时间
+    # while (time.time() - time_start) < runtime:
+    #     # "我要吐了" * 555
+    #     pass
+    # time.sleep(sleep_time)
+
     while True:
         time_start = time.time()  # 记录开始时间
         while (time.time() - time_start) < runtime:
             pass
         time.sleep(sleep_time)
         if usage_list[cpu_index] > 35:
-            if runtime <= 0:
-                # 运行时间最小是0
-                continue
-            print(f"cpu{cpu_index}大于35")
+            print("大于35")
             sleep_time = sleep_time + 0.01
             runtime = runtime - 0.01
             pass
         if usage_list[cpu_index] < 25:
-            if runtime >= 100:
-                # 运行时间最大是100
-                continue
-            print(f"cpu{cpu_index}小于25")
+            print("小于25")
             sleep_time = sleep_time - 0.01
             runtime = runtime + 0.01
             pass
@@ -46,13 +50,12 @@ def occupy_cpu(percent, cpu_index, usage_list):
 usage_list = multiprocessing.Array('d', [66] * psutil.cpu_count())  # 存cpu每个核心的利用率
 
 if __name__ == "__main__":
-    cpu_percent = 30
+    total_percent = 30
     # 获取逻辑 CPU 的数量
     cpu_count = psutil.cpu_count()
 
     # 创建多个进程，每个进程都绑定到不同的 CPU 上运行
     processes = []
-
     # 计算cpu每个核心的利用率，
     process = multiprocessing.Process(target=get_usage_list, args=(usage_list,))
     process.start()
@@ -61,7 +64,7 @@ if __name__ == "__main__":
     # 占用cpu
     # for cpu_index in range(cpu_count):
     for cpu_index in range(cpu_count - 1):  # 留1核
-        process = multiprocessing.Process(target=occupy_cpu, args=(cpu_percent, cpu_index, usage_list))
+        process = multiprocessing.Process(target=occupy_cpu, args=(total_percent, cpu_index, usage_list))
         process.start()
         processes.append(process)
     print("occupying...")
@@ -70,3 +73,21 @@ if __name__ == "__main__":
         process.join()
 
     print('end')
+
+    # 计算cpu每个核心的利用率，
+    # process = multiprocessing.Process(target=get_usage_list, args=(usage_list,))
+    # process.start()
+    # processes.append(process)
+    #
+    # # 占用cpu
+    # # for cpu_index in range(cpu_count):
+    # for cpu_index in range(cpu_count-1): # 留1核
+    #     process = multiprocessing.Process(target=occupy_cpu, args=(cpu_percent, cpu_index, usage_list))
+    #     process.start()
+    #     processes.append(process)
+    # print("occupying...")
+    # # 等待所有进程结束
+    # for process in processes:
+    #     process.join()
+    #
+    # print('end')
--- test.py
+++ test.py
@@ -1,64 +1,11 @@
 # coding: utf-8
+import datetime
 import multiprocessing
 import time
 
 import psutil
 
-usage_list = multiprocessing.Array('d', [66] * psutil.cpu_count())
-
-
-def get_usage_list(usage_list):
-    # global usage_list
-    while True:
-        # 获取每个 CPU 的使用率，并存储在共享数组中的不同元素中
-        cpu_percent = psutil.cpu_percent(interval=0.5, percpu=True)
-        for i, percent in enumerate(cpu_percent):
-            # print(percent)
-            usage_list[i] = percent
-
-
-# def get_usage_list(usage_list):
-#     # global usage_list
-#     while True:
-#         usage_list = psutil.cpu_percent(interval=0.5,percpu=True)
-
-
-# 用于消耗特定cpu资源
-def occupy_cpu(percent, cpu_index, usage_list):
-    p = psutil.Process()
-    p.cpu_affinity([cpu_index])
-    fulltime = 1
-    runtime = 0.3
-    sleeptime = fulltime - runtime
-    while True:
-        time_start = time.time()  # 记录开始时间
-        while (time.time() - time_start) < runtime:
-            # print(time.time() - time_start)
-            # "我要吐了" * 555
-            pass
-        time.sleep(sleeptime)
-
-
-if __name__ == "__main__":
-    # 获取逻辑 CPU 的数量
-    cpu_count = psutil.cpu_count()
-
-    # 创建多个进程，每个进程都绑定到不同的 CPU 上运行
-    processes = []
-    #
-    # process = multiprocessing.Process(target=get_usage_list, args=(usage_list,))
-    # # process = multiprocessing.Process(target=get_usage_list, )
-    # process.start()
-    # processes.append(process)
-
-    for cpu_index in range(cpu_count):
-        process = multiprocessing.Process(target=occupy_cpu, args=(50, cpu_index, usage_list))
-        process.start()
-        processes.append(process)
-    #
-    # # while True:
-    # #     print(usage_list)
-    #
-    # 等待所有进程结束
-    # for process in processes:
-    #     process.join()
+p = psutil.Process()
+p.cpu_affinity([3])
+while True:
+    pass
