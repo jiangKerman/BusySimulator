d41c2dd8af31f0440e0f6b613f5b73a5653cfc39
jiangKerman <jiangkerman@outlook.com>
2024-04-03 18:52:14 +0800

    把波动范围添加进配置文件

--- main.py
+++ main.py
@@ -26,20 +26,49 @@ def occupy_memory(target_percent, interval=300):
         time.sleep(interval)
 
 
+def get_usage_list(usage_list):
+    # global usage_list
+    while True:
+        # 获取每个 CPU 的使用率，并存储在共享数组中的不同元素中
+        cpu_percent = psutil.cpu_percent(interval=1, percpu=True)
+        for i, percent in enumerate(cpu_percent):
+            usage_list[i] = percent
+
+
 # 用于消耗特定cpu资源
-def occupy_cpu(percent, cpu_index, ):
+def occupy_cpu(percent, error_range, cpu_index, usage_list):
     p = psutil.Process()
     p.cpu_affinity([cpu_index])
     full_time = 1  # 运行周期
     runtime = percent / 100
     sleep_time = full_time - runtime
+    max_percent = percent + error_range
+    min_percent = percent - error_range
+
     while True:
         time_start = time.time()  # 记录开始时间
         while (time.time() - time_start) < runtime:
-            # "我要吐了" * 555
             pass
         time.sleep(sleep_time)
+        if usage_list[cpu_index] > max_percent:
+            if runtime <= 0:
+                # 运行时间最小是0
+                continue
+            print(f"cpu{cpu_index}大于{max_percent}")
+            sleep_time = sleep_time + 0.01
+            runtime = runtime - 0.01
+            pass
+        if usage_list[cpu_index] < min_percent:
+            if runtime >= 100:
+                # 运行时间最大是100
+                continue
+            print(f"cpu{cpu_index}小于{min_percent}")
+            sleep_time = sleep_time - 0.01
+            runtime = runtime + 0.01
+            pass
 
+
+usage_list = multiprocessing.Array('d', [66] * psutil.cpu_count())  # 存cpu每个核心的利用率
 
 if __name__ == "__main__":
     # windows独有的进程问题
@@ -48,6 +77,7 @@ if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read('config.ini')
     cpu_percent = config.getint("settings", "cpu_percent")
+    error_range = config.getint('settings', 'error_range')
     memory_percent = config.getint("settings", "memory_percent")
 
     # 获取逻辑 CPU 的数量
@@ -60,12 +90,18 @@ if __name__ == "__main__":
     process.start()
     processes.append(process)
 
+    # 计算cpu每个核心的利用率，
+    process = multiprocessing.Process(target=get_usage_list, args=(usage_list,))
+    process.start()
+    processes.append(process)
+
     # 占用cpu
     for cpu_index in range(cpu_count):
-        process = multiprocessing.Process(target=occupy_cpu, args=(cpu_percent, cpu_index,))
+        process = multiprocessing.Process(target=occupy_cpu, args=(cpu_percent, error_range, cpu_index, usage_list))
         process.start()
         processes.append(process)
     print("occupying...")
     # 等待所有进程结束
     for process in processes:
         process.join()
+    print('end')
